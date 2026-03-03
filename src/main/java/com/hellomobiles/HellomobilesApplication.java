c9dbd164a203ff4a7d020dc485b22977dff0f706
Benill <benil45c@gmail.com>
2026-03-03 10:16:37 +0530

    Initial Commit 2.0

---
+++ src/main/java/com/hellomobiles/HellomobilesApplication.java
@@ -0,0 +1,13 @@
+package com.hellomobiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellomobilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellomobilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellomobiles/HellomobilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellomobiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellomobilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
